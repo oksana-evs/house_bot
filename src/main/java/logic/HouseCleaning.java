61670c6840249412999ceda3301c7568be5360e9
Oksana <moiseeva.ox@gmail.com>
2020-11-28 15:13:32 +0100

    properties

--- src/main/java/logic/HouseCleaning.java
+++ src/main/java/logic/HouseCleaning.java
@@ -1,5 +1,11 @@
 package logic;
 
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 public enum HouseCleaning {
     VACUUMING("/vacuum"),
     SINK_WASHING("/sink"),
@@ -8,7 +14,9 @@ public enum HouseCleaning {
     DUST_OFF("/dust");
 
     private final String name;
-    private boolean isDone;
+
+    public static final String FILE_NAME = "state.properties";
+    private static final Logger LOGGER = Logger.getLogger(HouseCleaning.class.getName());
 
     HouseCleaning(String name) {
         this.name = name;
@@ -19,16 +27,36 @@ public enum HouseCleaning {
     }
 
     public boolean isDone() {
-        return isDone;
+        String done = "false";
+        Properties props = new Properties();
+        try (FileReader reader = new FileReader(FILE_NAME)) {
+            props.load(reader);
+            done = props.getProperty(name, String.valueOf(false));
+        } catch (IOException exception) {
+            LOGGER.severe(exception.getMessage());
+        }
+        return Boolean.parseBoolean(done);
     }
 
     public void setDone(boolean done) {
-        isDone = done;
+        Properties props = new Properties();
+        try (FileReader reader = new FileReader(FILE_NAME)) {
+            props.load(reader);
+            props.setProperty(name, String.valueOf(done));
+        } catch (IOException exception) {
+            LOGGER.severe(exception.getMessage());
+        }
+        try (FileOutputStream out = new FileOutputStream(FILE_NAME)) {
+            props.setProperty(name, String.valueOf(done));
+            props.store(out, null);
+        } catch (IOException exception) {
+            LOGGER.severe(exception.getMessage());
+        }
     }
 
     public static void reset() {
         for (HouseCleaning cleaning : HouseCleaning.values()) {
-            System.out.printf("Setting %s to done\n", cleaning.getName());
+            LOGGER.info(String.format("Setting %s to not done\n", cleaning.getName()));
             cleaning.setDone(false);
         }
     }
